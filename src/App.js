52b1e2d8177fd00ded7c39f6e3805a30590b33df
Alexandr Zhelanov <>
2024-02-27 20:42:43 +0300

    Revert index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDScD9zYs8V9fh4EFJaAUxwW6DVRO5fW80",
+  authDomain: "quizface-45e19.firebaseapp.com",
+  projectId: "quizface-45e19",
+  storageBucket: "quizface-45e19.appspot.com",
+  messagingSenderId: "831115215424",
+  appId: "1:831115215424:web:77aee1b76632b3ed3095a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const tg = window.Telegram.WebApp;
 
 function App() {
